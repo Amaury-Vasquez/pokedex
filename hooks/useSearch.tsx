3938d6f738bb674db0c751bab429478deb386885
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-22 09:36:26 -0500

    Added custom hook for search input

---
+++ hooks/useSearch.tsx
@@ -0,0 +1,16 @@
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
+
+export const useSearch = (callback: Function) => {
+  // State
+  const [value, setValue] = useState('');
+  // Functions
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const input = e.target.value;
+    setValue(input);
+    callback(input);
+  };
+  // Effects
+
+  return { value, handleChange };
+};
