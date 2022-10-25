3d277d62d4cd7d1dc5e52f597eaae23fe657b998
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-25 11:41:22 -0500

    Created hook for handling pokemon data

---
+++ hooks/usePokemon.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+
+import { usePokemonImages } from './usePokemonImages';
+import { PokemonData } from 'interfaces/pokemonInterfaces';
+// import { fetchUrl } from 'utils/functions';
+
+export const usePokemon = (name: string | string[] | undefined) => {
+  // Constants
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+
+  // State
+  const [data, setData] = useState<PokemonData | undefined>(undefined);
+  const [error, setError] = useState<unknown | null>(null);
+  const [attempts, setAttemtps] = useState(0);
+  const { artwork, spritesImages } = usePokemonImages(data?.id, data?.sprites);
+  // const [evolutionChain, setEvolutions] = useState();
+
+  // Functions
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch(url);
+      await res.json().then((poke) => {
+        setData(poke);
+        setError(null);
+      });
+    } catch (e) {
+      setError(e);
+    }
+  };
+
+  // Effects
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  useEffect(() => {
+    let render = true;
+
+    if (render) {
+      if (attempts < 10 && !data && error) {
+        setTimeout(() => {
+          fetchPokemon();
+          setAttemtps(attempts + 1);
+        }, 3000);
+      }
+    }
+
+    return () => {
+      render = false;
+    };
+  }, [attempts, data, error]);
+
+  // useEffect(() => {
+  //   if (data) {
+  //     const fetchEvolveChain = async () => {
+  //       try {
+  //         const species = await fetchUrl(data.species.url);
+  //         const evolutionChain = await fetchUrl(species.evolution_chain.url);
+  //         console.log(evolutionChain);
+  //       } catch {
+  //         console.log('Error getting pokemon forms');
+  //       }
+  //     };
+  //     fetchEvolveChain();
+  //     // console.log(data);
+  //   }
+  // }, [data]);
+
+  return { data, artwork, spritesImages };
+};
