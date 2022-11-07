880c00260f0a295c4427bb6291f554a8ab0e67be
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-11-06 20:59:50 -0600

    Move component to pokedex folder

---
+++ components/Pokedex/searchBar.tsx
@@ -0,0 +1,29 @@
+import { AiOutlineSearch } from 'react-icons/ai';
+
+import { useSearch } from 'hooks/useSearch';
+import styles from 'styles/pokedex.module.css';
+import animation from 'styles/animation.module.css';
+
+const { wrapper, bar, icon, input } = styles;
+
+export const SearchBar = (props: { callback: Function }) => {
+  const { callback } = props;
+  const { value, handleChange } = useSearch(callback);
+
+  return (
+    <div className={`${wrapper} ${animation.fadeIn}`}>
+      <div className={bar}>
+        <span className={icon}>
+          <AiOutlineSearch />
+        </span>
+        <input
+          className={input}
+          value={value}
+          type="text"
+          placeholder="mew"
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+};
